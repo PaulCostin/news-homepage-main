d323f8ac917a5f63f4113c7689b2073b9a847576
Paul Costin <Paul@Vivobook.local>
2023-01-09 20:56:28 +0200

    FINAL COMMIT

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-function toggleMenu() {}
+let navMenu = document.getElementById("navigationMenu");
+
+function toggleMenu() {
+  if (navMenu.classList.contains("hidden")) {
+    navMenu.classList.remove("hidden");
+    navMenu.classList.add("shown");
+    document.body.style.setProperty("overflow", "hidden");
+    document.body.classList.add("overlay");
+  } else {
+    navMenu.classList.add("hidden");
+    navMenu.classList.remove("shown");
+    document.body.style.removeProperty("overflow", "hidden");
+    document.body.classList.remove("overlay");
+  }
+}
